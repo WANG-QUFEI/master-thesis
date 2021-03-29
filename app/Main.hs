6bc2d8f3e703fc9c05857bddc73dfe9ddff2595e
WANG-QUFEI <wangqufei2009@gmail.com>
2021-03-29 11:57:13 +0200

    synchronize work

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import System.Environment ( getArgs, getProgName )
 import System.Exit        ( exitFailure, exitSuccess )
+import System.Directory   ( doesFileExist )
 import Control.Monad      ( when )
 import Debug.Trace
 
@@ -13,22 +14,56 @@ import Core.Layout ( resolveLayout )
 import TypeChecker
 
 type Err = Either String
+
 type ParseFun a = [Token] -> Err a
+
+type Verbosity = Int
+
+type REPL_State = Int
 
 myLLexer = resolveLayout True . myLexer
 
-type Verbosity = Int
+main :: IO ()
+main = do
+  putStrLn "\n*** Command Line Interface (CLI) for Loading a File and Producing a Computing Context for the Language SiminiTT ***\n"
+  args <- getArgs
+  case args of
+    []           -> usage
+    "--help":_   -> usage
+    ["-v"]       -> usage
+    "-v":file:_  -> do
+      fileExist <- doesFileExist file
+      if fileExist
+        then loadFile 2 pContext file >>= repl 0
+        else putStrLn "error: file does not exist" >> exitFailure
+    file:_       -> do
+      fileExist <- doesFileExist file
+      if fileExist
+        then loadFile 0 pContext file >>= repl 0
+        else putStrLn "error: file does not exist" >> exitFailure
+
+usage :: IO ()
+usage = do
+  putStrLn $ unlines
+    [ "usage: siminitt [Option] FilePath"
+    , ""
+    , "Options: "
+    , "  --help          display this help message"
+    , "  -v              display verbose information"
+    , ""
+    , "FilePath:         path of the file to be loaded"]
+  exitFailure
 
 putStrV :: Verbosity -> String -> IO ()
 putStrV v s = when (v > 1) $ putStrLn s
 
-runFile :: (Print a, Show a) => Verbosity -> ParseFun a -> FilePath -> IO ()
-runFile v p f = readFile f >>= run v p
+loadFile :: (Print a, Show a) => Verbosity -> ParseFun a -> FilePath -> IO Cont
+loadFile v p f = readFile f >>= load v p
 
-run :: (Print a, Show a) => Verbosity -> ParseFun a -> String -> IO ()
-run v p s = case p ts of
+load :: (Print a, Show a) => Verbosity -> ParseFun a -> String -> IO Cont
+load v p s = case p ts of
     Left s -> do
-      putStrLn "\nParse              Failed...\n"
+      putStrLn "\n\9889\9889\9889 PARSE FAILED \9889\9889\9889\n"
       putStrV v "Tokens:"
       putStrV v $ show ts
       putStrLn s
@@ -38,8 +73,8 @@ run v p s = case p ts of
       printSource v src
       let Right p = pContext ts
       case runTypeCheckCtx p of
-        Left err -> printError err
-        Right _  -> printSuccess "TYPE CHECK SUCCESS" >> exitSuccess
+        Left err -> printError err >> exitFailure
+        Right c  -> printSuccess "TYPE CHECK SUCCESS" >> exitSuccess >> return c
   where
   ts = myLLexer s
 
@@ -63,27 +98,9 @@ printError err = do
      (h : tail) -> do
        let ts' = fmap (\x -> " \10008 " ++ x) ts
        mapM_ putStrLn ts'
-       exitFailure
 
 printSuccess :: String -> IO ()
-printSuccess s = putStrLn ("\9972 " ++ s ++ " \9972")
+printSuccess s = putStrLn ("\n\9972 " ++ s ++ " \9972\n")
 
-usage :: IO ()
-usage = do
-  putStrLn $ unlines
-    [ "usage: siminitt [Option] File"
-    , "Options: "
-    , "  --help          Display this help message"
-    , "  -p              print parsing result"
-    ]
-  exitFailure
-
-main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [] -> putStrLn "invalid argument!" >> usage
-    "--help":_   -> usage
-    "-p":file:_  -> runFile 2 pContext file
-    file:_       -> runFile 0 pContext file
-  
+repl :: REPL_State -> Cont -> IO ()
+repl = undefined
--- app/TypeChecker.hs
+++ app/TypeChecker.hs
@@ -1,17 +1,9 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module TypeChecker (
     TypeCheckError
-  , Exp(..)
-  , Val
-  , Env(..)
-  , Cont(..)
-  , G
+  , Cont
   , runTypeCheckCtx
   , errorText
-  , runG
-  , eval
-  , appVal
-  , freshVar
   ) where
 
 import Data.Maybe
@@ -328,6 +320,47 @@ runTypeCheckCtx ctx@(Ctx cs) =
       let ss = ["when checking: " ++ (printTree (cs !! n)), "         decl: " ++ show d]
       throwError $ ExtendedErr err ss
 
+-- | get the environment related with a context by giving a list of unlocked definitions
+envContLock :: [String] -> Cont -> Env
+envContLock _ CNil = ENil
+envContLock xs (CConsVar c _ _) = envContLock xs c
+envContLock xs (CConsDef c x a e) =
+  let r = envContLock xs c in
+    if x `elem` xs
+    then EConsDef r x a e
+    else r
 
+-- | get the definition of a variable
+defCont :: String -> Cont -> Exp
+defCont x CNil = Var x
+defCont x (CConsVar c x' a)
+  | x == x'   = Var x
+  | otherwise = defCont x c
+defCont x (CConsDef c x' a e)
+  | x == x'   = e
+  | otherwise = defCont x c
 
+-- | head reduction evaluation
+headRed :: Cont -> Exp -> Val
+headRed c (Var x) = eval (defCont x c) ENil
+headRed c (App e1 e2) = appVal (headRed c e1) (eval e2 ENil)
+headRed c e = eval e ENil
 
+-- | turn a value into an expression, remove the closure of a value
+readBack :: [String] -> Val -> Exp
+readBack _ U = U
+readBack _ (Var x) = Var x
+readBack ns (App v1 v2) = App (readBack ns v1) (readBack ns v2)
+readBack ns (Clos (Abs x a e) r) =
+  let z  = freshVar x ns
+      a' = readBack ns (eval a r)
+      e' = readBack (z : ns) (eval e (EConsVar r x (Var z)))
+  in Abs z a' e'
+
+-- | variables in a context
+varsCont :: Cont -> [String]
+varsCont c = map fst (typeCont c)
+
+-- | evaluate an expression in an incremental way 
+incrEval :: Cont -> Exp -> Exp
+incrEval c e = readBack (varsCont c) (headRed c e)
